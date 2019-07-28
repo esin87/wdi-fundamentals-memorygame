af9317177c104e8f42655b8c21d77806f9a0d5b7
Esin Saribudak <esin@Esins-MacBook-Air.local>
2019-07-28 12:28:44 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -5,7 +5,26 @@ var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 //Will hold cards in play.
 
-const cardOne = cards[0];
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[2]) {
+  	console.log("You found a match!");
+} else {
+  	console.log("Sorry, try again.");
+}
+};
+
+function flipCard(cardId){
+	console.log('User flipped ' + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+			checkForMatch();
+	}
+}
+
+flipCard(0);
+flipCard(2);
+
+/*const cardOne = cards[0];
 cardsInPlay.push('cardOne');
 
 console.log('User flipped queen');
@@ -14,14 +33,13 @@ console.log('User flipped queen');
 const cardTwo = cards[2];
 cardsInPlay.push('cardTwo');
 //Adds cardTwo to end of cards in play array.
-
 console.log('User flipped king');
 //Displays second card that user flipped.
 
-if (cardsInPlay.length === 2 && cardsInPlay[0]===cardsInPlay[2]) {
+checkForMatch deleted: 
+	if (cardsInPlay.length === 2 && cardsInPlay[0]===cardsInPlay[2]) {
 	alert('You found a match!'); 
 } else {
 	alert('Sorry, try again.');
 }
-//Checks to see if the length of the cardsInPlay array is 2
-
+*/
