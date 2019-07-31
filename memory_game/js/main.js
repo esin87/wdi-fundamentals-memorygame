ed678b03857fed7a44dd7bc83eb8c2c4a0b1b9ff
Esin Saribudak <esin@Esins-Air.attlocal.net>
2019-07-31 14:01:12 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,3 +1,4 @@
+console.log("Up and running!");
 
 var cards = [
 {
@@ -26,24 +27,40 @@ var cards = [
 var cardsInPlay = [];
 //Will hold cards in play.
 
+
+function flipCard(){
+	var cardId = this.getAttribute('data-id');
+	console.log('User flipped ' + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+			checkForMatch();
+	};
+};
+
+console.log(cardsInPlay);
+
 function checkForMatch() {
-	if (cardsInPlay[0] === cardsInPlay[2]) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
   	alert('You found a match!');
 } else {
   	alert('Sorry, try again.');
 }
 };
 
-function flipCard(cardId){
-	console.log('User flipped ' + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suite);
-	cardsInPlay.push(cards[cardId].rank);
-	if (cardsInPlay.length === 2) {
-			checkForMatch();
-	};
-}
+//not sure if the below code is correct. Directions: "For each card, use createElement to create an img element and store it in a variable cardElement"
+function createBoard() {
+		const cardTable = document.getElementById('game-board');
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		cardTable.appendChild(cardElement);
+	}
+};
 
-flipCard(0);
-flipCard(2);
+
+
+createBoard();
 
